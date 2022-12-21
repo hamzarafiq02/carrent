a30b137b270cf9f6a3df2da13d2246d0c7aa205b
hamzarafiq02 <hamzarafiq9876@gmail.com>
2022-12-21 14:54:21 +0500

    done vercel setting

--- build_files.sh
+++ build_files.sh
@@ -2,12 +2,12 @@
 
 echo "Installing dependencies..."
 
-python3.11 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "Migrating database..."
 
-python3.11 manage.py makemigrations --noinput
-python3.11 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Creating superuser..."
 
@@ -15,11 +15,11 @@ DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL}
 DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME}
 DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD}
 
-python3.11 manage.py createsuperuser \
+python3.9 manage.py createsuperuser \
     --email $DJANGO_SUPERUSER_EMAIL \
     --username $DJANGO_SUPERUSER_USERNAME \
     --noinput || true
 
 echo "Collecting static files..."
 
-python3.11 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
